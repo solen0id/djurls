c33ce3f598c21537de3ac7d502cbbff8e85c9a78
Max Patzelt <max.patzelt@momox.biz>
2020-12-29 16:51:05 +0100

    Add default redirect view

--- djurls/urls.py
+++ djurls/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic.base import RedirectView
 from shorty import views
 
 urlpatterns = [
+    path("", RedirectView.as_view(pattern_name="url_list_create")),
     path("admin/", admin.site.urls),
     path("api/urls/", views.ShortURLListCreateAPI.as_view(), name="url_list_create"),
 ]
