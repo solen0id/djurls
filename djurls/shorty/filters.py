ea621cb58d52c2765dde61f5ad7de25d979d8bdd
Max Patzelt <max.patzelt@momox.biz>
2021-01-24 17:08:07 +0100

    Add docstring to filter backend

--- djurls/shorty/filters.py
+++ djurls/shorty/filters.py
@@ -2,6 +2,11 @@ from rest_framework.filters import BaseFilterBackend
 
 
 class IsAuthenticatedUserFilterBackend(BaseFilterBackend):
+    """
+    Filters a queryset based on the user making the request.
+
+    """
+
     def filter_queryset(self, request, queryset, view):
         if request.user.is_authenticated:
             return queryset.filter(author=request.user)
